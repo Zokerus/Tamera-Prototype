4834898610f2a60dc5645a3cd106e4ecfc514cbc
Zokerus <Zokerus@users.noreply.github.com>
2025-01-01 12:53:55 +0100

    adjust player movement based on camera

--- Character/Player/player.gd
+++ Character/Player/player.gd
@@ -3,9 +3,16 @@ class_name PlayerCharacter
 
 @export var mainCamera: CameraController
 
-const SPEED = 5.0
+const SPEED = 3.0
 const JUMP_VELOCITY = 4.5
 const CHARACTER_ROTATION_RATE = 4*PI;
+
+var movementDirection: Vector3 = Vector3.ZERO
+
+func _ready() -> void:
+	#Quit game if main camera is not ready yet
+	if !mainCamera:
+		get_tree().quit()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,15 +23,22 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	CalculateMovement(delta)
+	if movementDirection != Vector3.ZERO:
+		velocity.x = movementDirection.x * SPEED
+		velocity.z = movementDirection.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	mainCamera.position = self.position
+
+	move_and_slide()
+
+func CalculateMovement(delta: float) -> void:
+	var input_dir: Vector2 = Input.get_vector("Left", "Right", "Forward", "Backward")
+	movementDirection = (mainCamera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if movementDirection != Vector3.ZERO:
+		#var rotation_Angle: float = -transform.basis.z.signed_angle_to(movementDirection, Vector3.UP)
+		self.rotation.y = lerp_angle(self.rotation.y, atan2(-movementDirection.x, -movementDirection.z), CHARACTER_ROTATION_RATE * delta)
