ec79229ffbf1137d58f9612f92f22321e55127e7
Zokerus <Zokerus@users.noreply.github.com>
2025-01-02 14:52:15 +0100

    updated camera to Over-Shoulder-Camera

--- Character/Camera/camera_controller.gd
+++ Character/Camera/camera_controller.gd
@@ -1,7 +1,8 @@
 extends Node3D
 class_name CameraController
 
-@onready var spring_arm_3d: SpringArm = $SpringArm3D
+@onready var spring_arm_3d: SpringArm3D = $SpringArm3D2
+@onready var v_pivot: Node3D = $vPivot
 
 @export var h_Sensetivity: float = 0.005
 @export var v_Sensetivity: float = 0.005
@@ -15,5 +16,5 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		#var mouseMotion: InputEventMouseMotion = event
 		self.rotate_y(-event.relative.x * h_Sensetivity) #turn horizontally around the player
-		spring_arm_3d.rotate_x(-event.relative.y * v_Sensetivity) #turn vertically around the player
-		spring_arm_3d.rotation.x = clampf(spring_arm_3d.rotation.x, -PI*0.25, PI*0.25) 
+		v_pivot.rotate_x(-event.relative.y * v_Sensetivity) #turn vertically around the player
+		v_pivot.rotation.x = clampf(v_pivot.rotation.x, -PI*0.25, PI*0.25) 
