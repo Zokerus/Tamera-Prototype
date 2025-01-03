4377a8186d226ad03f157b056da3edb0c82feda1
Zokerus <Zokerus@users.noreply.github.com>
2025-01-03 18:16:02 +0100

    fix detection bug
    interactable items name will be printed in debug when detected be ShapeCast3D

--- Character/Camera/camera_controller.gd
+++ Character/Camera/camera_controller.gd
@@ -4,6 +4,7 @@ class_name CameraController
 @onready var v_pivot: Node3D = $vPivot
 @onready var shape_cast_3d: ShapeCast3D = $vPivot/SpringArm3D/Node3D/Camera3D/ShapeCast3D
 
+signal interactable_detected(detect: bool)
 
 @export var h_Sensetivity: float = 0.005
 @export var v_Sensetivity: float = 0.005
@@ -21,16 +22,16 @@ func _unhandled_input(event: InputEvent) -> void:
 		v_pivot.rotation.x = clampf(v_pivot.rotation.x, -PI*0.25, PI*0.25) 
 
 func _physics_process(delta: float) -> void:
-	var object: Interactable = Get_Interactable()
+	var object: InteractComponent = Get_Interactable()
 	if object:
-		print(object.name)
+		print(object.get_parent().name)
 	
-func Get_Interactable()-> Interactable:
+func Get_Interactable()-> InteractComponent:
 	for i in shape_cast_3d.get_collision_count():
 		if i > 0 and shape_cast_3d.get_collider(i) is PlayerCharacter:
 			# ShapeCast3D detect the player itself
 			return null
-		if shape_cast_3d.get_collider(i).get_node_or_null("InteractComponent") is Interactable:
+		if shape_cast_3d.get_collider(i).get_node_or_null("InteractComponent") is InteractComponent:
 			return shape_cast_3d.get_collider(i).get_node_or_null("InteractComponent")
 	
 	return null
