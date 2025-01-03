0a8fc8bd7369da518953ec32d5cfa1c1d6696cc6
Zokerus <Zokerus@users.noreply.github.com>
2025-01-03 23:23:16 +0100

    adjust chest interaction logic

--- World/Objects/chest.gd
+++ World/Objects/chest.gd
@@ -3,10 +3,20 @@ class_name Chest
 
 @onready var animPlayer: AnimationPlayer = $AnimationPlayer
 
+var status: bool = false
+
+func _interact()-> void:
+	if status:
+		_close()
+	else:
+		_open()
+
 func _open()-> void:
+	status = true
 	animPlayer.stop()
 	animPlayer.play("Open")
 
 func _close()-> void:
+	status = false
 	animPlayer.stop()
 	animPlayer.play("Close")
