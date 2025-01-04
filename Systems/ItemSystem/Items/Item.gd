e1d9c7b3da50c5d2894d39036f6be21b2fce4b52
Zokerus <Zokerus@users.noreply.github.com>
2025-01-04 14:09:11 +0100

    add generic item resource

---
+++ Systems/ItemSystem/Items/Item.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name Item
+
+@export var Name: String
+@export_multiline var Description: String = ""
+@export var Value: int = 0
+@export var Icon: Texture2D
+@export var Body: PackedScene
