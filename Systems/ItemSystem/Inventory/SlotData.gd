6ac9f54be7d2ce89ffccaa9bf4e4d55e1f6d3419
Zokerus <Zokerus@users.noreply.github.com>
2025-01-05 11:35:35 +0100

    add stackable property to ItemData
    - Add Stackable Property (bool) to ItemDara
    - check in SlotData if Stackable and quantity are contradicting

--- Systems/ItemSystem/Inventory/SlotData.gd
+++ Systems/ItemSystem/Inventory/SlotData.gd
@@ -2,4 +2,9 @@ extends Resource
 class_name SlotData
 
 @export var item_data: ItemData
-@export var quantity: int
+@export var quantity: int = 1: set = set_quantity
+
+func set_quantity(value: int) -> void:
+	quantity = value
+	if quantity > 1 and !item_data.Stackable:
+		quantity = 1
--- Systems/ItemSystem/Items/ItemData.gd
+++ Systems/ItemSystem/Items/ItemData.gd
@@ -4,5 +4,6 @@ class_name ItemData
 @export var Name: String = ""
 @export_multiline var Description: String = ""
 @export var Value: int = 0
+@export var Stackable: bool = true
 @export var Icon: AtlasTexture
 @export var Body: PackedScene
