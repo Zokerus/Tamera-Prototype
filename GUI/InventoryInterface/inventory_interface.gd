eede7ed09b71170bf8f6ab7768d91ae1b9f3f31f
Zokerus <Zokerus@users.noreply.github.com>
2025-01-07 20:35:27 +0100

    send interacted slot data to UI element

--- GUI/InventoryInterface/inventory_interface.gd
+++ GUI/InventoryInterface/inventory_interface.gd
@@ -4,4 +4,8 @@ class_name InventoryInterface
 @onready var player_inventory: Inventory = $PlayerInventory
 
 func set_player_inventory(data: InventoryData)-> void:
+	data.slot_interact.connect(on_slot_interact)
 	player_inventory.fill_inventory(data)
+
+func on_slot_interact(slot_data: SlotData, index: int, button: int)-> void:
+	print("%s %s %s" % [slot_data, index, button])
--- Systems/ItemSystem/Inventory/InventoryData.gd
+++ Systems/ItemSystem/Inventory/InventoryData.gd
@@ -3,5 +3,7 @@ class_name InventoryData
 
 @export var slot_data: Array[SlotData]
 
+signal slot_interact(slot_data: SlotData, index: int, button: int)
+
 func on_slot_clicked(index: int, button: int)-> void:
-	print("Slot clicked")
+	slot_interact.emit(self.slot_data[index], index, button)
