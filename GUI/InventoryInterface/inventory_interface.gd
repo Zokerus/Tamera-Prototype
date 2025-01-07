e7da1e5acb34ad34c614abf1db48941c53bc0b09
Zokerus <Zokerus@users.noreply.github.com>
2025-01-07 21:55:16 +0100

    pick up slot into grabbed slot andrefactored some code

--- GUI/InventoryInterface/inventory_interface.gd
+++ GUI/InventoryInterface/inventory_interface.gd
@@ -2,10 +2,28 @@ extends Control
 class_name InventoryInterface
 
 @onready var player_inventory: Inventory = $PlayerInventory
+@onready var grabbed_slot: Slot = $GrabbedSlot
+
+var grabbed_slot_data: SlotData  #May be deleted
+
+func _process(delta: float) -> void:
+	if grabbed_slot.visible:
+		grabbed_slot.global_position = get_global_mouse_position() + Vector2(5,5)
 
 func set_player_inventory(data: InventoryData)-> void:
-	data.slot_interact.connect(on_slot_interact)
+	data.inventory_interact.connect(on_inventory_interact)
+	data.inventory_updated.connect(player_inventory.fill_inventory)
 	player_inventory.fill_inventory(data)
 
-func on_slot_interact(slot_data: SlotData, index: int, button: int)-> void:
-	print("%s %s %s" % [slot_data, index, button])
+func set_grabbed_slot()-> void:  #may use varibles
+	if grabbed_slot_data != null:
+		grabbed_slot.set_slot_data(grabbed_slot_data)
+		grabbed_slot.show()
+	else:
+		grabbed_slot.hide()
+
+func on_inventory_interact(inventory_data: InventoryData, index: int, button: int)-> void:
+	if grabbed_slot_data == null and button == MOUSE_BUTTON_LEFT:
+		grabbed_slot_data = inventory_data.grab_slot_data(index)
+		
+	set_grabbed_slot()
--- Systems/ItemSystem/Inventory/InventoryData.gd
+++ Systems/ItemSystem/Inventory/InventoryData.gd
@@ -3,7 +3,18 @@ class_name InventoryData
 
 @export var slot_data: Array[SlotData]
 
-signal slot_interact(slot_data: SlotData, index: int, button: int)
+signal inventory_interact(inventory_data: InventoryData, index: int, button: int)
+signal inventory_updated(inventory_data)
 
 func on_slot_clicked(index: int, button: int)-> void:
-	slot_interact.emit(self.slot_data[index], index, button)
+	inventory_interact.emit(self, index, button)
+
+func grab_slot_data(index: int)-> SlotData:
+	var data: SlotData = slot_data[index]
+	
+	if data != null:
+		slot_data[index] = null
+		inventory_updated.emit(self)
+		return data
+	else:
+		return null
